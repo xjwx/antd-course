4284002604a317023a11fdae51f1612d4748f458
IreneXJW <xjwx3320@163.com>
2020-04-28 16:14:54 +0800

    init

--- src/.umi/core/plugin.ts
+++
@@ -1,7 +0,0 @@
-import { Plugin } from '/Users/xujingwen/gitskills/antd-course/node_modules/_@umijs_runtime@3.1.1@@umijs/runtime';
-
-const plugin = new Plugin({
-  validKeys: ['patchRoutes','rootContainer','render','onRouteChange',],
-});
-
-export { plugin };
